b6448a9aba5950d74c55de933b73c7710023492b
amine <mo.amine.alt@gmail.com>
2025-08-06 19:45:49 +0100

    Add Header component and styles; update App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Header';
+
+
 
 function App() {
   return (
     <div className="App">
         <h1>let's build the amazon store</h1>
+        <Header />
+        {/* {Home} */}
     </div>
   );
 }
