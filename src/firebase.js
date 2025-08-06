6017a777c0422b4ad4b86939a4bb4e1e73839f5d
amine <mo.amine.alt@gmail.com>
2025-08-06 19:14:21 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWfNSVG63M9m0WlS14cn85lPmrrGxRDX8",
+  authDomain: "clone-6f203.firebaseapp.com",
+  projectId: "clone-6f203",
+  storageBucket: "clone-6f203.firebasestorage.app",
+  messagingSenderId: "484817421290",
+  appId: "1:484817421290:web:aef468cbecf4c9da87a893",
+  measurementId: "G-VQMNRY5F3C"
+};
