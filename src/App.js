e25333b1bff7f34898e4e6c8c339b37ea7e04686
amine <mo.amine.alt@gmail.com>
2025-08-11 01:39:07 +0100

    Add favicon and update login button styles for improved UI

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <Routes>
+                    <Route path='/login' element={<Login />} />                        
+                </Routes>
                 <Header/>
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/checkout' element={<Checkout />} />
-                    <Route path='/login' element={<Login />} />                        
                 </Routes>
             </div>
         </BrowserRouter>
