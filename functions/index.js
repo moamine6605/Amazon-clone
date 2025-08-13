04b95c369f56cc081c31fbe79aed3ddfcb8797fc
amine <mo.amine.alt@gmail.com>
2025-08-13 02:04:38 +0100

    style: format code in index.js for consistency and readability

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(process.env.REACT_APP_STRIPE_API_KEY)
+// const stripe = require("stripe")(process.env.REACT_APP_STRIPE_API_KEY)
 
 // API
-
-
 
 // APP config
 
@@ -13,13 +11,14 @@ const app = express();
 
 // middlewares
 
-app.use(cors({origin:true}));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // api rouutes
 
-app.get('/', (request, response)=>response.status(200).send('hello world'))
+app.get("/", (request, response)=>response.status(200).send("hello world"));
 
 // listen command
 
-express.api = functions.https.onRequest(app)
+express.api = functions.https.onRequest(app);
+
